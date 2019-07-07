d7eb1d8fc3191ee8067276d558615287ac3e312d
bacnv <nguyenvanbac33@gmail.com>
2019-07-07 14:33:42 +0700

    recommit

--- src/main/java/com/t3h/demo/Main.java
+++ src/main/java/com/t3h/demo/Main.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
 public class Main {
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
